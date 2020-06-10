85fa0d04ac5f0fa6886e1a97acdd52c0675f11b4
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-11 03:18:38 +0500

    Update code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState}  from 'react';
+
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    let [count, setCount] = useState(100);
+    let [isLite, setDay] = useState(false)
+    
+    return (
+    
+    <div className={`box ${isLite? "dayLight" : "nightLight"}`}>
+    {/* <Msg counter={count}/>    */}
+
+    <h1>Value of counters is {count}</h1>
+    
+    
+    <button className="Button" onClick={
+        ()=> setCount(++count) 
+    }> 
+     Update Counter</button>
+
+    <hr />
+    <br />
+
+    <h1>Day time: {isLite ? 'Morning' : 'Night'}</h1>
+    
+     <br />
+    
+    <button className="Button" onClick={()=> setDay(!isLite)}> Set Light</button>
     </div>
-  );
+    );
 }
 
 export default App;
